3f8eb640fa7fed6ccbf7e95122ba1a3fcba12742
darinetag <tagdarinefatma@gmail.com>
2023-09-09 14:53:51 +0100

    assignment completed

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+ 
+return (
+  <>
+ <div>Naturally, I agree with this article.</div>
+ <div>I love this article.</div>
+ <div> Bjarne Stroustrup ROCKS </div>
+  </>
+)
 }
 
 export default Comment;
